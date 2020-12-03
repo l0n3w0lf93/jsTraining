f5670dd0b5ecb7a271e5cdd20bb71eb86f3752c0
gamotapcode <thanhltptit@gmail.com>
2020-12-03 10:47:47 +0700

    tim hieu li thuyet

---
+++ unit1.js
@@ -0,0 +1,26 @@
+// Variable
+// phân biệt các biến: scope,let,const,var
+// dùng khi nào? phạm vi ảnh hưởng của các biến
+var a = 1
+let b = 2
+function main() {
+    console.log('a', a)
+    console.log('b', b)
+}
+//Data type
+// liệt kê các kiểu dữ liệu của js
+// cách check kiểu dữ liệu trong js
+// phân biệt undefined, null,0,""
+console.log('"1"+1=', "1" + 1)
+console.log("1" == 1)
+console.log("1" === 1)
+
+// Object la gi?
+// cách gán/lấy giá trị 1 thuộc tính trong object
+// tìm hiểu hàm JSON.parse hàm JSON.stringify. lấy ví dụ
+
+//khai báo 1 Array
+// viết chương trình in ra các phần tử trong mảng
+// ghép mảng, cắt mảng
+// thêm bớt phần tử trong mảng
+// so sánh các hàm map, forEach, for, for .. in
